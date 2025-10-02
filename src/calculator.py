f7d26b086fd4bdab5d4a07b4e7ad4b74088aaa58
Danila <Ephirious@gmail.com>
2025-10-02 22:41:01 +0300

    refactor: refactor code by previous change

--- src/calculator.py
+++ src/calculator.py
@@ -1,107 +1,155 @@
 from src.tokens import Token, Number, Operator
 
-class Calculator():
+
+class Calculator:
     tokens: list[Token]
     tokens_length: int
     pos: int
-    
+    log: str
+
     def __init__(self):
         self.tokens = None
         self.tokens_length = 0
         self.pos = 0
+        self.log = ""
 
-    def calculate(self, tokens: list[Token]):
+    def calculate(self, tokens: list[Token]) -> int | float:
         self.tokens = tokens
         self.tokens_length = len(self.tokens)
         self.pos = 0
+        self.log = ""
 
-        return self.expr()
+        result = round(self._expr(), 2)
 
-    def current_token(self) -> Token:
-        if self.has_next():
+        return result
+
+    def _current_token(self) -> Token:
+        if self._has_next():
             return self.tokens[self.pos]
         raise Exception("No such tokens")
-    
-    def next_pos(self) -> None:
+
+    def _next_pos(self) -> None:
         self.pos += 1
 
-    def has_next(self):
-        if (self.pos < self.tokens_length):
+    def _has_next(self):
+        if self.pos < self.tokens_length:
             return True
         return False
 
-    def expr(self) -> int:
-        return self.add()
+    def _expr(self) -> int | float:
+        return self._add()
 
-    def add(self) -> int:
-        left = self.mul()
-        
-        while self.has_next() and (token := self.current_token()) and (token.get_token() in (Operator.PLUS, Operator.MINUS)):
-            self.next_pos()
-            right = self.mul()
+    def _add(self) -> int | float:
+        left = self._mul()
 
-            if (token.get_token() is Operator.PLUS):
+        while (
+            self._has_next()
+            and (token := self._current_token())
+            and (token.get_token() in (Operator.PLUS, Operator.MINUS))
+        ):
+            self.log += token.get_token()
+            self._next_pos()
+            right = self._mul()
+
+            if token.get_token() is Operator.PLUS:
                 left += right
             else:
                 left -= right
 
         return left
 
-    def mul(self) -> int:
-        left = self.pow()
+    def _mul(self) -> int | float:
+        left = self._pow()
 
-        while self.has_next() and (token := self.current_token()) and \
-            (token.get_token() in (Operator.MULTIPLICATION, Operator.DIVISION, Operator.INTEGER_DIVISION, Operator.REMAINDER_DIVISION)):
-            self.next_pos()
-            right = self.pow()
+        while (
+            self._has_next()
+            and (token := self._current_token())
+            and (
+                token.get_token()
+                in (
+                    Operator.MULTIPLICATION,
+                    Operator.DIVISION,
+                    Operator.INTEGER_DIVISION,
+                    Operator.REMAINDER_DIVISION,
+                )
+            )
+        ):
+            self.log += token.get_token()
+            self._next_pos()
+            right = self._pow()
 
-            match(token.get_token()):
+            match token.get_token():
                 case Operator.MULTIPLICATION:
                     left *= right
                 case Operator.DIVISION:
+                    self._check_zero_division(right)
                     left /= right
                 case Operator.INTEGER_DIVISION:
+                    self._check_zero_division(right)
+                    self._check_integer_types(left, right)
                     left //= right
                 case Operator.REMAINDER_DIVISION:
+                    self._check_zero_division(right)
+                    self._check_integer_types(left, right)
                     left %= right
-        
+
         return left
 
-    def pow(self) -> int:
-        left = self.unary()
+    def _pow(self) -> int | float:
+        left = self._unary()
 
-        while self.has_next() and (token := self.current_token()) and (token.get_token() is Operator.POWER):
-            self.next_pos()
-            right = self.pow()
-            
+        while (
+            self._has_next()
+            and (token := self._current_token())
+            and (token.get_token() is Operator.POWER)
+        ):
+            self.log += token.get_token()
+            self._next_pos()
+            right = self._pow()
+
             left **= right
 
         return left
 
-    def unary(self) -> int:
-        token = self.current_token()
+    def _unary(self) -> int | float:
+        token = self._current_token()
 
         is_operator = token.get_token() in (Operator.PLUS, Operator.MINUS)
+
         if is_operator:
-            self.next_pos()
-            value = self.unary()
-            
+            self.log += token.get_token()
+            self._next_pos()
+            value = self._unary()
+
             if token.get_token() is Operator.PLUS:
                 return +value
             else:
                 return -value
 
         else:
-            return self.primary()
-                
-    def primary(self) -> int:
-        if self.has_next():
-            token = self.current_token()
-            self.next_pos()
-            
+            return self._primary()
+
+    def _primary(self) -> int | float:
+        if self._has_next():
+            token = self._current_token()
+            self.log += token.get_token()
+            self._next_pos()
+
             if isinstance(token, Number):
+                if token.is_float():
+                    return float(token.get_token())
                 return int(token.get_token())
+
             elif token.get_token() is Operator.LEFT_BRACKET:
-                result = self.expr()
-                self.next_pos()
+                result = self._expr()
+                self.log += self._current_token().get_token()
+                self._next_pos()
                 return result
+
+    def _check_zero_division(self, right):
+        if right == 0:
+            raise ZeroDivisionError(self.log)
+
+    def _check_integer_types(self, left, right):
+        if isinstance(left, float) or isinstance(right, float):
+            raise ArithmeticError(self.log)
