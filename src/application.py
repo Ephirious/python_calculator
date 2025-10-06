0fb7b37a357134ff27b2755812e85f67f336ed79
Danila <Ephirious@gmail.com>
2025-10-06 16:32:02 +0300

    docs: add docString for application.py

--- src/application.py
+++ src/application.py
@@ -1,3 +1,7 @@
+"""
+Модуль приложения
+"""
+
 from src.tokenizer import Tokenizer
 from src.validator import Validator
 from src.calculator import Calculator
@@ -6,22 +10,49 @@ from src.tokens import Token
 
 
 class Application:
+    """
+    Класс-приложения
+
+    Атрибуты:
+        validator (Validator): валидатор
+        tokenizer (Tokenizer): токенизатор
+        calculator (Calculator): калькулятор
+    """
+
     validator: Validator
     tokenizer: Tokenizer
     calculator: Calculator
 
-    def __init__(self, validator, tokenizer, calculator):
+    def __init__(
+        self, validator: Validator, tokenizer: Tokenizer, calculator: Calculator
+    ) -> None:
+        """
+        Внедрение зависимостей
+        """
         self.validator = validator
         self.tokenizer = tokenizer
         self.calculator = calculator
 
-    def execute(self):
+    def execute(self) -> None:
+        """
+        Функция, запускаяющая работу приложения
+
+        Вовзращаемое значение:
+            None
+        """
         self._start_main_loop()
 
-    def _start_main_loop(self):
-        PROGRAMM_QUESTION = "Please write your expression: "
+    def _start_main_loop(self) -> None:
+        """
+        Вспомогательная функция, организующая запуск главного цикла программы
 
-        while (user_input := input(PROGRAMM_QUESTION)) != "q":
+        Возвращаемое значение:
+            None
+        """
+        PROGRAMM_QUESTION = "Введите ваше выражение: "
+        STOP_SYMBOL = "q"
+
+        while (user_input := input(PROGRAMM_QUESTION)) != STOP_SYMBOL:
             result = None
 
             try:
@@ -31,10 +62,10 @@ class Application:
             except ExpressionError as exception:
                 print(exception)
             except ZeroDivisionError as exception:
-                print("Запрещено делить на ноль.")
+                print("Запрещено делить на ноль")
                 print(f"Эта часть выражения вызывает ошибку -> '{exception.args[0]}'")
             except TypeError as exception:
-                print("Операнд слева должен быть целым для операция % и //.")
+                print("Операнд слева и справа должны быть целыми для операция % и //")
                 print(f"Эта часть выражения вызывает ошибку -> '{exception.args[0]}'")
             except DigitsOverFlow as exception:
                 print(f"Слишком большие размеры операндов -> {exception.log}")
@@ -42,6 +73,16 @@ class Application:
                 self._output_result(tokens, result)
 
     def _output_result(self, tokens: list[Token], result: int | float) -> None:
+        """
+        Вспомогательная функция для удобного отображения результатов вычислений
+
+        Аргументы:
+            tokens (list[Token]): токены выражения
+            result (int | float): результат выражения
+
+        Возвращаемое значение:
+            None
+        """
         for token in tokens:
             print(token.get_token(), end=" ")
         print(f"= {result}")
